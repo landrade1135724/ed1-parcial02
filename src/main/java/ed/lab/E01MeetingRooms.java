7763c017a86bfb4ef0b0b35571a010be7f3911ea
Luis Andrade <laandradeg@correo.url.edu.gt>
2025-05-23 08:06:24 -0600

    Ejercicios Completados

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,11 +1,29 @@
 package ed.lab;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class E01MeetingRooms {
-
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        return -1;
-    }
+        if (meetingIntervals == null || meetingIntervals.isEmpty()) {
+            return 0;
+        }
 
+        meetingIntervals.sort(Comparator.comparingInt(MeetingInterval::startTime));
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        minHeap.add(meetingIntervals.get(0).endTime());
+
+        for (int i = 1; i < meetingIntervals.size(); i++) {
+            MeetingInterval current = meetingIntervals.get(i);
+            if (current.startTime() >= minHeap.peek()) {
+                minHeap.poll();
+            }
+            minHeap.add(current.endTime());
+        }
+
+        return minHeap.size();
+    }
 }
--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -1,11 +1,21 @@
 package ed.lab;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class E02Logger {
+    private Map<String, Integer> nextAllowed;
 
     public E02Logger() {
+        nextAllowed = new HashMap<>();
     }
 
     public boolean shouldPrintMessage(int timestamp, String message) {
+        Integer allowTime = nextAllowed.get(message);
+        if (allowTime == null || timestamp >= allowTime) {
+            nextAllowed.put(message, timestamp + 10);
+            return true;
+        }
         return false;
     }
 
